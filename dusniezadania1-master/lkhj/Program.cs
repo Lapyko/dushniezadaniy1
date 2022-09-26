4421307335929a878982a931b6fe936f045b8810
Lapyko <113054648+Lapyko@users.noreply.github.com>
2022-09-26 18:00:56 +0300

    Add files via upload

---
+++ dusniezadania1-master/lkhj/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lkhj
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Printable[] printables = new Printable[5];
+            printables[1] = new Book("книга первая");
+            printables[2] = new Magazine("журнал первый");
+            printables[3] = new Book("книга вторая");
+            printables[4] = new Magazine("журнал второй");
+
+            foreach (Printable printable in printables)
+            {
+                printable.Print();
+            }
+            
+                Book.PrintBooks(printables);
+                Console.WriteLine("это книга");
+            
+            
+                Magazine.PrintMagazine(printables);
+                Console.WriteLine("это журнал");
+            
+            Console.ReadKey();
+        }
+    }
+}
